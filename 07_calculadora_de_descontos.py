25b85e35ae23ec2f09b9a3881fcd75c2b47e0568
Milton Bezerra Filho <miltonbfilho206@gmail.com>
2025-07-15 20:24:13 -0300

    Update 07_calculadora_de_descontos.py

--- 07_calculadora_de_descontos.py
+++ 07_calculadora_de_descontos.py
@@ -4,7 +4,7 @@ formato = str( input("Digite a forma de pagamento: "))
 if (formato == "Cartão" or formato == "cartão"):
     print("Formato de pagamento inválido.")
 elif (formato == "Cheque" or formato == "cheque"):
-    print(f'O preço é R${valor}')
+    print(f'O preço é R$ {valor}')
 elif (formato == "Dinheiro" or formato == "dinheiro"):
     if (valor <= 99.9):
         print(f'O preço é R$ {valor}')
