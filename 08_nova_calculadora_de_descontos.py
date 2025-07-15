72061d5eb3cc95167aeae69fceb3fb2bc829cf31
Milton Bezerra Filho <miltonbfilho206@gmail.com>
2025-07-15 20:23:31 -0300

    Update 08_nova_calculadora_de_descontos.py

--- 08_nova_calculadora_de_descontos.py
+++ 08_nova_calculadora_de_descontos.py
@@ -2,7 +2,7 @@ valor = float( input("Digite o valor: "))
 formato = str( input("Digite a forma de pagamento: "))
 
 if (formato == "Cheque" or formato == "cheque"):
-    print(f'O preço é R${valor}')
+    print(f'O preço é R$ {valor}')
 elif (formato == "Dinheiro" or formato == "dinheiro"):
     if (valor <= 99.9):
         print(f'O preço é R$ {valor}')
